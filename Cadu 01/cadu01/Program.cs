bf0c9826e027ebeec42a1b93556e755f30c4e65f
Gabriel Costa <gabrielpcll@hotmail.com>
2023-01-17 17:55:51 -0300

    frist commit

---
+++ Cadu 01/cadu01/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Principal;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cadu01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string numero;
+            Console.WriteLine("Digite um número:");
+            numero = Console.ReadLine();
+            Console.WriteLine();
+
+            Console.WriteLine("O número digitado foi " + numero);
+            Console.WriteLine();
+
+
+        }
+    }
+}
---
+++ Cadu 02/Cadu 02/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cadu_02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int numero;
+
+            Console.WriteLine("Digite um número:");
+            numero = Convert.ToInt32(Console.ReadLine());
+
+            if (numero == 0)
+            {
+
+                Console.WriteLine("O número é igual a Zero");
+
+            }
+
+            else
+            {
+                Console.WriteLine("O numero não é igual a Zero");
+            }
+
+
+        }
+    }
+}
